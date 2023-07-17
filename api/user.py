83fcf6d53272ca177b3ac6826edc0a820480d5a7
Sheng <elio.sheng@easyhealthcarecorp.com>
2023-07-17 18:32:37 +0800

    delete file

--- api/user.py
+++ api/user.py
@@ -7,30 +7,6 @@ data_file_path = os.path.join(BASE_PATH, "config", "setting.ini")
 api_root_url = data.load_ini(data_file_path)["host"]["api_root_url"]
 
 
-class User(RestClient):
-
-    def __init__(self, api_root_url, **kwargs):
-        super(User, self).__init__(api_root_url, **kwargs)
-
-    def list_all_users(self, **kwargs):
-        return self.get("/users", **kwargs)
-
-    def list_one_user(self, username, **kwargs):
-        return self.get("/users/{}".format(username), **kwargs)
-
-    def register(self, **kwargs):
-        return self.post("/register", **kwargs)
-
-    def login(self, **kwargs):
-        return self.post("/login", **kwargs)
-
-    def update(self, user_id, **kwargs):
-        return self.put("/update/user/{}".format(user_id), **kwargs)
-
-    def delete(self, name, **kwargs):
-        return self.post("/delete/user/{}".format(name), **kwargs)
-
-
 class UserService(RestClient):
     def __init__(self, api_root_url, **kwargs):
         super(UserService, self).__init__(api_root_url, **kwargs)
@@ -45,5 +21,4 @@ class UserService(RestClient):
         return self.post("/user/web/sign/up", **kwargs)
 
 
-user = User(api_root_url)
 userservice = UserService(api_root_url)
